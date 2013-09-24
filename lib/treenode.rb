842ce887b4f26401f7d42307f1b50d2c7b43baa0
Adler Santos <adlersantos712@gmail.com>
2013-09-24 10:40:33 -0700

    new implementation of bfs and dfs with arbitrary number of children

--- lib/treenode.rb
+++ lib/treenode.rb
@@ -1,21 +1,74 @@
 class TreeNode
-  attr_accessor :parent, :value, :left, :right
+  attr_accessor :parent, :children, :value
 
-  def initialize(value)
-    @parent, @value = nil, value
+  def initialize(value=nil)
+    @parent = nil
+    @children = []
+    @value = value
   end
 
-  def left=(child_node)
-    self.left.parent = nil if self.left
-
-    @left = child_node
-    child_node.parent = self
+  def children=(child_nodes)
+    @children = child_nodes
+    child_nodes.each do |child_node|
+      child_node.parent = self
+    end
   end
 
-  def right=(child_node)
-    self.right.parent = nil if self.right
+  # depth-first search
+  def dfs(end_value=nil, &block)
+    if block && block.call(@value)
+      return self
+    elsif @value == end_value
+      return self
+    else
+      @children.each do |child|
+        temp = child.dfs(end_value, &block)
+        return temp if temp
+      end
+    end
+    nil
+  end
 
-    @right = child_node
-    child_node.parent = self
+  # breadth-first search
+  def bfs(end_value=nil, &block)
+    nodes_to_check = [self]
+    next_nodes_to_check = []
+
+    visited_nodes = []
+
+    while !nodes_to_check.empty?
+      nodes_to_check.each do |node|
+        if block && block.call(node.value)
+          return node
+        elsif node.value == end_value
+          return node
+        else
+          if !visited_nodes.include?(node)
+            node.children.each do |child|
+              next_nodes_to_check << child
+            end
+
+            visited_nodes << node
+          end
+        end
+      end
+
+      nodes_to_check = next_nodes_to_check
+      next_nodes_to_check = []
+    end
+    nil
   end
 end
+
+# a = TreeNode.new(1)
+# b = TreeNode.new(2)
+# c = TreeNode.new(3)
+# d = TreeNode.new(4)
+# e = TreeNode.new(5)
+# f = TreeNode.new(3)
+
+# a.children = [b, c]
+# b.children = [d, e, f]
+
+# a.bfs { |value| value == 3 } => c
+# a.dfs { |value| value == 3 } => f
